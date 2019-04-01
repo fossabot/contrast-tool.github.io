d4433d8db64a1981944b7c12567c4aef7a5ea149
Rikkun Brouwers <rikkunbrouwers@gmail.com>
2019-04-01 10:05:47 +0200

    -

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","355a86a12f9361383c655d9a95d0282a"],["manifest.json","2eb4da43e883477fac65b1ef472a121c"],["src/applic.js","4a2e343196575de91ae259840cd34467"],["src/applic.lazies.js","1617b2b3eabcbff4275f249d406794f0"],["src/units/processing/applic.processing.js","d41d8cd98f00b204e9800998ecf8427e"]];
+var precacheConfig = [["index.html","c43b41df7c9d2166141c9c59c6dda6b3"],["manifest.json","2eb4da43e883477fac65b1ef472a121c"],["src/applic.js","4a2e343196575de91ae259840cd34467"],["src/applic.lazies.js","1617b2b3eabcbff4275f249d406794f0"],["src/units/processing/applic.processing.js","d41d8cd98f00b204e9800998ecf8427e"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
